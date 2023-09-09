36c4529c6fe47471905751f69dd592d4f377fd33
JuaniLlaberia <juanillaberia2002@gmail.com>
2023-09-09 17:04:45 -0300

    Refactor error handling

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,11 +1,19 @@
 const AppError = require('../utils/appError');
 
-const sendErrorDev = (err, res) => {
-  res.status(err.statusCode).json({
-    status: err.status,
-    error: err,
-    message: err.message,
-    stack: err.stack,
+const sendErrorDev = (err, req, res) => {
+  //API
+  if (req.originalUrl.startsWith('/api')) {
+    return res.status(err.statusCode).json({
+      status: err.status,
+      error: err,
+      message: err.message,
+      stack: err.stack,
+    });
+  }
+  //RENDERED FRONT END
+  return res.status(err.statusCode).render('error', {
+    title: 'Something went wrong!',
+    msg: err.message,
   });
 };
 
@@ -31,24 +39,43 @@ const handleValidationErrorDB = (error) => {
   return new AppError(message, 400);
 };
 
-const sendErrorProd = (err, res) => {
-  //For our errors we set the custom response (TRUSTED ERROR)
-  if (err.isOperational) {
-    res.status(err.statusCode).json({
-      status: err.status,
-      message: err.message,
-    });
-  } else {
+const sendErrorProd = (err, req, res) => {
+  //API
+  if (req.originalUrl.startsWith('/api')) {
+    // A) Operational error (trusted)
+    if (err.isOperational) {
+      return res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message,
+      });
+    }
+    //B) Programming error/Unkown one
     //1) Log error:
     console.error('Error', err);
 
     //2) Generic errors that we dont have control such as libraries or smth we send a generic message(UNKOWN ERROR)
     //This is in order to not leak the error details to the client
-    res.status(500).json({
+    return res.status(500).json({
       status: 'error',
       message: 'Something went wrong!',
     });
   }
+
+  //RENDERED WEBSITE
+  //A) Operational
+  if (err.isOperational) {
+    console.log(err);
+    return res.status(err.statusCode).render('error', {
+      title: 'Something went wrong!',
+      msg: err.message,
+    });
+  }
+
+  //B)Unkown error
+  return res.status(err.statusCode).render('error', {
+    title: 'Something went wrong!',
+    msg: 'Please try again later.',
+  });
 };
 
 module.exports = (err, req, res, next) => {
@@ -57,10 +84,12 @@ module.exports = (err, req, res, next) => {
   err.status = err.status || 'error';
 
   if (process.env.NODE_ENV === 'development') {
-    sendErrorDev(err, res);
+    sendErrorDev(err, req, res);
   } else if (process.env.NODE_ENV === 'production') {
     //There are some mongoose errors that we dont create but the user needs to have a meaninful msg, not the default one
     let error = { ...err };
+    error.message = err.message;
+
     //CastError is a type of error from mongoose(sending a wrong ID in a route, for example the get tour)
     if (error.name === 'CastError') error = handleCastErrorDB(error);
     //This error is when the field already exist (duplicate name)
@@ -71,7 +100,7 @@ module.exports = (err, req, res, next) => {
     if (error.name === 'JsonWebTokenError') error = handleJWTError();
     if (error.name === 'TokenExpiredError') error = handleJWTExpiredError();
 
-    sendErrorProd(error, res);
+    sendErrorProd(error, req, res);
   }
 
   next();
